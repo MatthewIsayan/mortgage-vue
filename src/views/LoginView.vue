273b43139a89a71c1496436d01e93743a0676d93
Matthew <misayan@gmail.com>
2023-08-22 20:36:51 -0700

    Added login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login-layout">
+        <Card style="max-width: 450px">
+            <template #title> Quick Login </template>
+            <template #content>
+                <div style="display: flex; flex-direction: column; gap: 2rem">
+                    <Button
+                        @click="routeTo($event, 'GetQuote', 'Guest Borrower')"
+                        label="Login as Guest Borrower"
+                        size="small"
+                    ></Button>
+                    <Button
+                        @click="routeTo($event, 'GetQuote', 'Borrower')"
+                        label="Login as Borrower with Credentials"
+                        disabled
+                        size="small"
+                    ></Button>
+                    <Button
+                        @click="routeTo($event, 'Dashboard', 'Loan Officer')"
+                        label="Login as Loan Officer"
+                        size="small"
+                    ></Button>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+function routeTo(e: any, route: string, role: string) {
+    const { setUserID, userID } = useAuthStore();
+    setUserID(role);
+
+    router.push({ name: route });
+}
+</script>
+
+<style scoped>
+.login-layout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
